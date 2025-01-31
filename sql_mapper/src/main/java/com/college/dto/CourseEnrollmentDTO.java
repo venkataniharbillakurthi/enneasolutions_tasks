23dfddbfe89dfb79d6ce5bda5faacb143c384720
venkataniharbillakurthi <venkataniharbillakurthi@gmail.com>
2025-01-31 17:20:22 +0530

    update on sql mapper

--- sql_mapper/src/main/java/com/college/dto/CourseEnrollmentDTO.java
+++ sql_mapper/src/main/java/com/college/dto/CourseEnrollmentDTO.java
@@ -1,40 +1,13 @@
 package com.college.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CourseEnrollmentDTO {
     private String courseName;
-    private long studentCount; 
-  
-   
-
-    
-    public CourseEnrollmentDTO(String courseName, long studentCount) {
-        this.courseName = courseName;
-        this.studentCount = studentCount;
-       
-    }
-
-   
-
-    
-    public long getStudentCount() {
-        return studentCount;
-    }
-
-    public void setStudentCount(long studentCount) {
-        this.studentCount = studentCount;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-
-   
-
-    
+    private long studentCount;
 }
--- sql_mapper/target/generated-sources/annotations/com/college/mapper/CollegeMapperImpl.java
+++ sql_mapper/target/generated-sources/annotations/com/college/mapper/CollegeMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-31T11:26:02+0530",
+    date = "2025-01-31T17:17:27+0530",
     comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.41.0.z20250115-2156, environment: Java 21.0.5 (Eclipse Adoptium)"
 )
 @Component
